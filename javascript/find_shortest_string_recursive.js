7e953d58fc971e3ecaa337fd86704600520c7a95
Isaac <isaac.lin.pnc@gmail.com>
2023-08-01 17:25:15 -0400

    finished lab

--- javascript/find_shortest_string_recursive.js
+++ javascript/find_shortest_string_recursive.js
@@ -1,5 +1,13 @@
 function findShortestStringRecursive(arr) {
-  // type your code here
+  if (arr.length === 1) {
+    return arr[0];
+  }
+
+  const firstString = arr[0];
+  const shortestInRest = findShortestStringRecursive(arr.slice(1));
+
+  return firstString.length <= shortestInRest.length ? firstString : shortestInRest
+
 }
 
 if (require.main === module) {
